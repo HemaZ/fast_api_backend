4f15e8cf700b217a0d5a013fcc3dc7f3812ec26f
Ibrahim Essam <ibrahim.essam1995@gmail.com>
2024-01-30 16:05:49 +0000

    Add posts likes with tests

--- storeapi/routers/post.py
+++ storeapi/routers/post.py
@@ -1,21 +1,30 @@
 import logging
 from typing import Annotated
+from enum import Enum
+import sqlalchemy
+from fastapi import APIRouter, Depends, HTTPException, status
 
-from fastapi import APIRouter, Depends, HTTPException
-
-from storeapi.database import comment_table, database, post_table
+from storeapi.database import comment_table, database, post_table, like_table
 from storeapi.models.post import (
     Comment,
     CommentIn,
     UserPost,
     UserPostIn,
     UserPostWithComments,
+    PostLike,
+    PostLikeIn,
 )
 from storeapi.models.user import User
 from storeapi.security import get_current_user
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
+
+select_post_and_likes = (
+    sqlalchemy.select(post_table, sqlalchemy.func.count(like_table.c.id).label("likes"))
+    .select_from(post_table.outerjoin(like_table))
+    .group_by(post_table.c.id)
+)
 
 
 async def find_post(post_id: int):
@@ -36,9 +45,20 @@ async def create_post(
     return {"id": post_id, "body": post.body, "user_id": current_user.id}
 
 
+class PostSorting(str, Enum):
+    new = "new"
+    old = "old"
+    most_likes = "most_likes"
+
+
 @router.get("/post", response_model=list[UserPost])
-async def get_all_posts():
-    query = post_table.select()
+async def get_all_posts(sorting: PostSorting = PostSorting.new):
+    if sorting == PostSorting.new:
+        query = select_post_and_likes.order_by(post_table.c.id.desc())
+    elif sorting == PostSorting.old:
+        query = select_post_and_likes.order_by(post_table.c.id.asc())
+    elif sorting == PostSorting.most_likes:
+        query = select_post_and_likes.order_by(sqlalchemy.desc("likes"))
     logger.debug(query)
     return await database.fetch_all(query)
 
@@ -66,10 +86,29 @@ async def get_comments_on_post(post_id: int):
 
 @router.get("/post/{post_id}/", response_model=UserPostWithComments)
 async def get_post_with_comments(post_id: int):
-    post = await find_post(post_id)
+    query = select_post_and_likes.where(post_table.c.id == post_id)
+    logger.debug(query)
+    post = await database.fetch_one(query)
     if not post:
         raise HTTPException(status_code=404, detail="Post not found")
     return {
         "post": post,
         "comments": await get_comments_on_post(post_id),
     }
+
+
+@router.post("/like", response_model=PostLike, status_code=201)
+async def like_post(
+    like: PostLikeIn, current_user: Annotated[User, Depends(get_current_user)]
+):
+    logger.info("Liking post with Id:%d", like.post_id)
+    post = await find_post(like.post_id)
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
+        )
+    data = {**like.model_dump(), "user_id": current_user.id}
+    query = like_table.insert().values(data)
+    logger.debug(query)
+    like_id = await database.execute(query)
+    return {**data, "id": like_id}
--- tests/routers/test_post.py
+++ tests/routers/test_post.py
@@ -13,6 +13,17 @@ async def create_post(
     return response.json()
 
 
+async def like_post(
+    post_id: int, async_client: AsyncClient, logged_in_token: str
+) -> dict:
+    response = await async_client.post(
+        "/like",
+        json={"post_id": post_id},
+        headers={"Authorization": f"Bearer {logged_in_token}"},
+    )
+    return response
+
+
 @pytest.fixture()
 async def created_post(async_client: AsyncClient, logged_in_token: str):
     return await create_post("Test Post", async_client, logged_in_token)
@@ -23,6 +34,15 @@ async def test_create_new_post(async_client: AsyncClient, logged_in_token):
     body = "Test Post"
     response = await create_post(body, async_client, logged_in_token)
     assert {"id": 1, "body": body}.items() <= response.items()
+
+
+@pytest.mark.anyio
+async def test_like_post(
+    async_client: AsyncClient, logged_in_token, created_post: dict
+):
+    response = await like_post(created_post["id"], async_client, logged_in_token)
+    assert response.status_code == 201
+    assert {"id": 1, "post_id": created_post["id"]}.items() <= response.json().items()
 
 
 @pytest.mark.anyio
@@ -38,3 +58,16 @@ async def test_getting_all_posts(async_client: AsyncClient, created_post: dict):
     response = await async_client.get("/post")
     assert response.status_code == 200
     assert created_post.items() <= response.json()[0].items()
+
+
+@pytest.mark.parametrize("sorting,expected_out", [("new", [2, 1]), ("old", [1, 2])])
+@pytest.mark.anyio
+async def test_get_all_posts_sorting(
+    async_client: AsyncClient, logged_in_token, sorting, expected_out
+):
+    await create_post("Test Post 1", async_client, logged_in_token)
+    await create_post("Test Post 2", async_client, logged_in_token)
+    response = await async_client.get("/post", params={"sorting": sorting})
+    assert response.status_code == 200
+    out = [post["id"] for post in response.json()]
+    assert out == expected_out
